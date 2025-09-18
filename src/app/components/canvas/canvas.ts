460c04c8eabf9b5dddf083239ecab04f550a21e8
asija-p <apopovic3002@gmail.com>
2025-09-18 10:35:15 +0200

    added pen sensitivity

--- src/app/components/canvas/canvas.ts
+++ src/app/components/canvas/canvas.ts
@@ -10,8 +10,8 @@ import { fromEvent, merge, pairwise, switchMap, takeUntil } from 'rxjs';
 export class Canvas implements AfterViewInit {
   @ViewChild('canvas') public canvas!: ElementRef<HTMLCanvasElement>;
 
-  @Input() public width = 400;
-  @Input() public height = 400;
+  @Input() public width = 700;
+  @Input() public height = 700;
 
   private cx: CanvasRenderingContext2D | undefined;
 
@@ -24,12 +24,22 @@ export class Canvas implements AfterViewInit {
       throw new Error('Could not get canvas context');
     }
 
+    ['touchstart', 'touchmove', 'touchend', 'pointerdown', 'pointermove', 'pointerup'].forEach(
+      (ev) => {
+        canvasEl.addEventListener(ev, (e) => {
+          console.log(ev, e);
+        });
+      }
+    );
+
     canvasEl.width = this.width;
     canvasEl.height = this.height;
 
     this.cx.lineWidth = 3;
     this.cx.lineCap = 'round';
     this.cx.strokeStyle = '#000';
+    this.cx.fillStyle = '#ffffff';
+    this.cx.fillRect(0, 0, canvasEl.width, canvasEl.height);
 
     this.captureEvents(canvasEl);
   }
@@ -42,6 +52,7 @@ export class Canvas implements AfterViewInit {
 
     const mouseUp = merge(
       fromEvent<MouseEvent>(window, 'mouseup'),
+      fromEvent<MouseEvent>(window, 'mouseleave'),
       fromEvent<TouchEvent>(window, 'touchend'),
       fromEvent<TouchEvent>(window, 'touchcancel')
     );
@@ -54,16 +65,14 @@ export class Canvas implements AfterViewInit {
     mouseDown
       .pipe(
         switchMap((e) => {
+          const rect = canvasEl.getBoundingClientRect();
+          this.handleDraw(e, rect);
           return mouseMove.pipe(takeUntil(mouseUp), pairwise());
         })
       )
-      .subscribe((res: [MouseEvent | TouchEvent, MouseEvent | TouchEvent]) => {
+      .subscribe(([prev, curr]: [MouseEvent | TouchEvent, MouseEvent | TouchEvent]) => {
         const rect = canvasEl.getBoundingClientRect();
-
-        const prevPos = this.getPos(res[0], rect);
-        const currentPos = this.getPos(res[1], rect);
-
-        this.drawOnCanvas(prevPos, currentPos);
+        this.handleDrawPair(prev, curr, rect);
       });
   }
 
@@ -72,17 +81,22 @@ export class Canvas implements AfterViewInit {
       return {
         x: ev.clientX - rect.left,
         y: ev.clientY - rect.top,
+        pressure: (ev as any).pressure ?? 1,
       };
     } else {
       const touch = ev.changedTouches[0];
       return {
         x: touch.clientX - rect.left,
         y: touch.clientY - rect.top,
+        pressure: touch.force ?? 1,
       };
     }
   }
 
-  private drawOnCanvas(prevPos: { x: number; y: number }, currentPos: { x: number; y: number }) {
+  private drawOnCanvas(
+    prevPos: { x: number; y: number; pressure: number },
+    currentPos: { x: number; y: number; pressure: number }
+  ) {
     if (!this.cx) {
       return;
     }
@@ -94,7 +108,25 @@ export class Canvas implements AfterViewInit {
 
       this.cx.lineTo(currentPos.x, currentPos.y);
 
+      const baseWidth = 5;
+      this.cx.lineWidth = baseWidth * currentPos.pressure;
+
       this.cx.stroke();
     }
+  }
+
+  private handleDraw(ev: MouseEvent | TouchEvent, rect: DOMRect) {
+    const pos = this.getPos(ev, rect);
+    this.drawOnCanvas(pos, pos);
+  }
+
+  private handleDrawPair(
+    prev: MouseEvent | TouchEvent,
+    curr: MouseEvent | TouchEvent,
+    rect: DOMRect
+  ) {
+    const prevPos = this.getPos(prev, rect);
+    const currPos = this.getPos(curr, rect);
+    this.drawOnCanvas(prevPos, currPos);
   }
 }

d77af4163d7ef5f12c533fab5deed18bb96819a9
asija-p <apopovic3002@gmail.com>
2025-09-22 15:31:00 +0200

    navigator reacts to layers

--- src/app/components/navigator/navigator.ts
+++ src/app/components/navigator/navigator.ts
@@ -3,35 +3,19 @@ import { Store } from '@ngrx/store';
 import { selectCurrentSnapshot, selectLayers } from '../../store/drawing.selectors';
 import { Observable } from 'rxjs';
 import { Layer } from '../../models/layer';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navigator',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navigator.html',
   styleUrl: './navigator.scss',
 })
-export class Navigator implements AfterViewInit {
-  @ViewChild('previewCanvas', { static: true })
-  previewCanvas!: ElementRef<HTMLCanvasElement>;
+export class Navigator {
   layers$: Observable<Layer[]>;
+  trackById = (_: number, l: Layer) => l.id;
 
   constructor(private store: Store) {
     this.layers$ = store.select(selectLayers);
-  }
-
-  ngAfterViewInit() {
-    const canvas = this.previewCanvas.nativeElement;
-    const ctx = canvas.getContext('2d')!;
-
-    this.store.select(selectCurrentSnapshot).subscribe((snapshot) => {
-      if (snapshot) {
-        const img = new Image();
-        img.src = snapshot.canvasData;
-        img.onload = () => {
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-          ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-        };
-      }
-    });
   }
 }
